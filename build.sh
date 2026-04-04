dba018ebe0eb1a5183f4721b1fc0dd3efe917610
yuriko_aya <thechildoflily@gmail.com>
2026-04-04 07:25:56 +0700

    improve build script to automatically install

--- build.sh
+++ build.sh
@@ -3,14 +3,45 @@
 
 set -e
 
+install_path="/opt/gossh"
+path_owner="gossh"
+path_group="gossh"
+gossh_service="gossh.service"
+
+# Make sure the script is run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root or with sudo"
+    exit 1
+fi
+
 echo "Building Go SSH Web Terminal..."
 
 # Build for Linux AMD64
 GOOS=linux GOARCH=amd64 go build -o gossh -ldflags="-s -w"
+if [ $? -ne 0 ]; then
+    echo "Build failed!"
+    exit 1
+fi
 
-echo "Build complete: gossh"
-echo "Size: $(du -h gossh | cut -f1)"
-echo ""
-echo "To deploy to production server:"
-echo "1. Copy all files: scp -r gossh templates static config.yaml.example gossh.service deploy.sh user@server:/tmp/"
-echo "2. On server: cd /tmp && sudo ./deploy.sh"
+if [[ ! -d "$install_path" ]]; then
+    echo "Install directory $install_path does not exist. Skipping installation."
+    exit 0
+fi
+
+echo "Installing gossh to $install_path..."
+
+if systemctl is-active --quiet $gossh_service; then
+    echo "Stopping gossh service..."
+    systemctl stop $gossh_service
+    install -m 755 -o $path_owner -g $path_group gossh $install_path/gossh
+    systemctl start $gossh_service
+    echo "gossh service restarted successfully."
+else
+    install -m 755 -o $path_owner -g $path_group gossh $install_path/gossh
+    if systemctl is-enabled --quiet $gossh_service; then
+        systemctl start $gossh_service
+        echo "gossh service started successfully."
+    else
+        echo "Please enable the gossh service to start on boot: sudo systemctl enable $gossh_service"
+    fi
+fi
